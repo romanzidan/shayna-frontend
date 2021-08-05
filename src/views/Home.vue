7c51d7ae9d9fb358097e5966a19942c66dcb4cce
Roman Zidan Ramadhan <romanzidanramadhan@gmail.com>
2021-08-05 12:56:12 +0800

    set caraousel to hero section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="home">
     <Header/>
+
     <!-- Hero Section Begin -->
     <section class="hero-section">
-        <div class="hero-items owl-carousel">
-            <div class="single-hero-items set-bg" data-setbg="img/hero-1.jpg">
+        <carousel class="hero-items" :items="1" :nav="false">
+            <div class="single-hero-items set-bg" style="background-image: url('/img/hero-1.jpg')">
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-5">
@@ -18,7 +19,7 @@
                     </div>
                 </div>
             </div>
-            <div class="single-hero-items set-bg" data-setbg="img/hero-2.jpg">
+            <div class="single-hero-items set-bg" style="background-image: url('/img/hero-2.jpg')">
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-5">
@@ -32,7 +33,21 @@
                     </div>
                 </div>
             </div>
-        </div>
+            <div class="single-hero-items set-bg" style="background-image: url('/img/hero-3.jpg')">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <span>Bag,kids</span>
+                            <h1>Black friday</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                            </p>
+                            <a href="#" class="primary-btn">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </carousel>
     </section>
     <!-- Hero Section End -->
   </div>
